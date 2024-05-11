173550f9a06fa4790dd7dff853fc5883abda41d5
Ethan Rock <ethan.s.rock@student.uts.edu.au>
2024-05-11 16:59:14 +1000

    DAO

--- src/main/java/uts/isd/model/Customer.java
+++ src/main/java/uts/isd/model/Customer.java
@@ -1,18 +1,44 @@
 package uts.isd.model;
 
 public class Customer extends User {
-    private String address;
+    private String customerType;
+    private String shippingAddress;
+    private boolean accountActive;
+    private String paymentDetails;
 
-    public Customer(String email, String username, String password, String address) {
-        super(email, username, password);
-        this.address = address;
+    public Customer(String _userName, String _userEmail, String _password, String _userType, String _customerType, String shippingAddress, String _paymentDetails) {
+        super(_userName, _userEmail, _password, _userType);
     }
 
-    public void setAddress(String value) {
-        this.address = value;
+    public String getCustomerType() {
+        return customerType;
     }
 
-    public String getAddress() {
-        return this.address;
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public boolean isAccountActive() {
+        return accountActive;
+    }
+
+    public void setAccountActive(boolean accountActive) {
+        this.accountActive = accountActive;
+    }
+
+    public String getPaymentDetails() {
+        return paymentDetails;
+    }
+
+    public void setPaymentDetails(String paymentDetails) {
+        this.paymentDetails = paymentDetails;
     }
 }
