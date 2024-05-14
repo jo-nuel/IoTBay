e2aa0a44c762bf54a6a78128b042d7916acf5f65
Wai Tin Lau <161002277+MartinMartinLau@users.noreply.github.com>
2024-05-14 11:47:34 +1000

    Added Supplier

---
+++ src/main/java/uts/isd/model/Supplier.java
@@ -0,0 +1,60 @@
+package uts.isd.model;
+
+import java.io.Serializable;
+
+public class Supplier implements Serializable {
+    private String supplierID;
+    private String supplierName;
+    private String emailAddress;
+    private String phoneNum;
+    private boolean recordActive;
+
+    public Supplier(String supplierID, String supplierName, String emailAddress, String phoneNum,boolean recordActive) {
+        this.supplierID = supplierID;
+        this.supplierName = supplierName;
+        this.emailAddress = emailAddress;
+        this.phoneNum = phoneNum;
+        this.recordActive = recordActive;
+    }
+
+    public String getsupplierID() {
+        return supplierID;
+    }
+
+    public void setsupplierID(String supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public String getsupplierName() {
+        return supplierName;
+    }
+
+    public void setsupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getemailAddress() {
+        return emailAddress;
+    }
+
+    public void setemailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getphoneNum() {
+        return phoneNum;
+    }
+
+    public void setphoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public boolean isrecordActive() {
+        return recordActive;
+    }
+
+    public void setrecordActive(boolean recordActive) {
+        this.recordActive = recordActive;
+    }
+
+}
