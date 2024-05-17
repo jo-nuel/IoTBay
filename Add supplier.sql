c210e783e6ae1a40ad79304a0ead39bfc638cbe8
Wai Tin Lau <161002277+MartinMartinLau@users.noreply.github.com>
2024-05-17 17:18:14 +1000

    Add files via upload

---
+++ Add supplier.sql
@@ -0,0 +1,48 @@
+--INSERT SUPPLIERS
+INSERT INTO iotbay.supplier (supplierName,emailAddress,phoneNum,recordActive) VALUES
+('TechD', 'techd@example.com', '1234567890', 1),
+('ProsHardware', 'prosh@example.com', '0987654321', 1),
+('ITGadget', 'itgadget@example.com', '1234567890', 0),
+('BrightTech', 'brighttech@example.com', '0987654321', 0),
+('QuantumDevice', 'quantum@example.com', '1234567890', 1),
+('GlobalTech', 'globaltech@example.com', '0987654321', 1),
+('FutureTech', 'futureitech@example.com', '1234567890', 0),
+('Pinnacle', 'pinnacle@example.com', '0987654321', 0),
+('Apex Hardware', 'apexhd@example.com', '1234567890', 1),
+('Alpha Technology', 'alphat@example.com', '0987654321', 1),
+('OmegaSolutions', 'omegas@example.com', '1234567890', 0),
+('Zenith', 'zenith@example.com', '0987654321', 0),
+('PrimeIT', 'primeit@example.com', '1234567890', 1),
+('PeakTech', 'peaktech@example.com', '0987654321', 1),
+('VertexIT', 'vertexit@example.com', '1234567890', 0),
+('Summit Techology', 'summit@example.com', '0987654321', 1),
+('PinnacleDevice', 'pinnacledevice@example.com', '1234567890', 1),
+('ApexITDevice', 'apexit@example.com', '0987654321', 1),
+('ZenithHardware', 'zenithit@example.com', '1234567890', 1),
+('Prime Tech', 'primetech@example.com', '0987654321', 0);
+
+
+--INSERT STAFF USERS
+INSERT INTO iotbay.user (userName, userEmail, password, userType) VALUES
+('Martin', 'martin@example.com', '1233ffd', 'Staff'),
+('Mary', 'mary@example.com', '5hgf5', 'Staff'),
+('James', 'james@example.com', '3545y', 'Staff'),
+('Patricia', 'patricia@example.com', '56gh', 'Staff'),
+('Robert', 'robert@example.com', '56ghb', 'Staff'),
+('Jennifer', 'jennifer@example.com', '5464dg', 'Staff'),
+('Michael', 'michael@example.com', '5465yh', 'Staff'),
+('Linda', 'linda@example.com', 'uyik8', 'Staff'),
+('William', 'william@example.com', 'gert54', 'Staff'),
+('Elizabeth', 'elizabeth@example.com', 'rfg5y55', 'Staff'),
+('David', 'david@example.com', '5tg4tr', 'Staff'),
+('Barbara', 'barbara@example.com', '456ygr', 'Staff'),
+('Richard', 'richard@example.com', '56ytrfg', 'Staff'),
+('Susan', 'susan@example.com', '546y5tdg', 'Staff'),
+('Joseph', 'joseph@example.com', 'yuji7u', 'Staff'),
+('Jessica', 'jessica@example.com', '123abC', 'Staff'),
+('Thomas', 'thomas@example.com', '4e5ters', 'Staff'),
+('Sarah', 'sarah@example.com', '4356t4e', 'Staff'),
+('Charles', 'charles@example.com', '34t435e', 'Staff'),
+('Karen', 'karen@example.com', '43t4re', 'Staff');
+
+ 
